af1f4ada11b317d69f57aa90cc065d7ccc069f34
ZacHooper <zac.g.hooper@gmail.com>
2021-10-20 16:08:25 +1100

    ENV vars now use Prefect secrets

--- flows/orchestration.py
+++ flows/orchestration.py
@@ -1,6 +1,7 @@
-from prefect import task, Flow
+from prefect import task, Flow, unmapped
 from prefect.storage.github import GitHub
 from prefect.run_configs.docker import DockerRun
+from prefect.tasks.secrets import PrefectSecret
 from DomainAnalysis.domain_api import get_listings_in_postcode, get_listing
 from DomainAnalysis.logger import logger
 from DomainAnalysis.mongo import (connect_to_mongo_db, connect_to_domain_raw_collection, 
@@ -10,13 +11,14 @@ from datetime import datetime
 from DomainAnalysis.wrangler import Listing
 import os
 from dotenv import load_dotenv
+import pymongo
+from pymongo import collection
 load_dotenv()
 
 @task
-def get_todays_listings_on_domain(postcode):
+def get_todays_listings_on_domain(domain_key, postcode):
     # get new listings
-    key = os.environ.get('DOMAIN_API_KEY')
-    listings = get_listings_in_postcode(key, postcode)
+    listings = get_listings_in_postcode(domain_key, postcode)
     if 'detail' in listings:
         if listings['detail'] == "Unable to verify credentials":
             logger.critical(f"Unable to verify domain credentials")
@@ -25,13 +27,8 @@ def get_todays_listings_on_domain(postcode):
     return listings
     
 @task
-def upload_raw_listings(data):
-    # connect to db
-    db_user = os.environ.get('MONGO_USERNAME')
-    db_password = os.environ.get('MONGO_PASSWORD')
-    client = connect_to_mongo_db(db_user, db_password)
+def upload_raw_listings(client, data):    
     collection = connect_to_domain_raw_collection(client)
-    
     # prepare data for upload
     listings_to_insert = {
         'listings': data,
@@ -46,11 +43,8 @@ def upload_raw_listings(data):
     return objectId
 
 @task
-def check_for_new_listings(listings):
+def check_for_new_listings(client, listings):
     # connect to db
-    db_user = os.environ.get('MONGO_USERNAME')
-    db_password = os.environ.get('MONGO_PASSWORD')
-    client = connect_to_mongo_db(db_user, db_password)
     collection = connect_to_domain_listings(client)
     logger.debug(f"Checking which listings are new. Listings: {listings}")
     new_listings = which_new_listings(collection, listings)
@@ -62,46 +56,64 @@ def get_listing_ids(listings):
     return [x['listing']['id'] for x in listings]
 
 @task
-def get_new_listing_details(listing_id):
-    key = os.environ.get('DOMAIN_API_KEY')
+def get_new_listing_details(domain_key, listing_id):
     logger.debug(f"Getting listing for listing_id: {listing_id}")
-    raw_listing = get_listing(key, listing_id)
+    raw_listing = get_listing(domain_key, listing_id)
     try:
         listing = Listing(raw_listing)
         return listing
     except Exception as e:
-        logger.critical(f"Unable to creating listing class due to: {e}")
+        logger.critical(f"Unable to create listing class due to: {e}")
 
 @task
-def add_new_listing_to_mongo(listing: Listing):
+def add_new_listing_to_mongo(client, listing: Listing):
     # connect to db
-    db_user = os.environ.get('MONGO_USERNAME')
-    db_password = os.environ.get('MONGO_PASSWORD')
-    client = connect_to_mongo_db(db_user, db_password)
     collection = connect_to_domain_listings(client)
+    # TODO: Add handle if listing is "NoneType"
     objectId = insert_into_collection(collection, listing.as_no_nested_dicts())
     if objectId:
         logger.info(f"Successfully inserted listing ({listing.listing_id}) into database")
     return objectId
+
+@task
+def connect_to_mongo(db_user, db_password):
+    """Connects to domain analysis mongo db
+
+    Args:
+        db_user (str): username
+        db_password (str): MongoDB password
+
+    Returns:
+        client: MongoDB database
+    """
+    return connect_to_mongo_db(db_user, db_password)
     
     
 
 with Flow("scrape-raw-from-domain") as flow:
-    listings = get_todays_listings_on_domain(3195)
-    objectId = upload_raw_listings(listings)
+    # Creds
+    db_user = PrefectSecret('MONGO_USERNAME')
+    db_password = PrefectSecret('MONGO_PASSWORD')
+    domain_key = PrefectSecret('DOMAIN_API_KEY')
+    
+    # Connect to Mongo
+    client = connect_to_mongo(db_user, db_password)
+    
+    listings = get_todays_listings_on_domain(domain_key, 3195)
+    objectId = upload_raw_listings(client, listings)
     # Check which listings are new
     listing_ids = get_listing_ids(listings)
-    new_listing_ids = check_for_new_listings(listing_ids)
+    new_listing_ids = check_for_new_listings(client, listing_ids)
     # Get details for new listings
-    new_listings = get_new_listing_details.map(new_listing_ids)
+    new_listings = get_new_listing_details.map(unmapped(domain_key), new_listing_ids)
     # Upload new listings to mongo
-    new_listing_objectIds = add_new_listing_to_mongo.map(new_listings)
+    new_listing_objectIds = add_new_listing_to_mongo.map(unmapped(client), new_listings)
     # Check which listings are sold
     
     # Update listing in mongo
     
-# flow.run()
+flow.run()
 
-flow.storage = GitHub(repo = "zachooper/domain-analysis", path="/flows/orchestration.py")
+flow.storage = GitHub(repo = "ZacHooper/realestate-analysis", path="/flows/orchestration.py")
 flow.run_config = DockerRun(image = "zhooper/domain-analysis")
 # flow.register(project_name="realestate-analysis")
